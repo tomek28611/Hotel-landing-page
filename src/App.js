fcef15c6879a37ab3b280e5660040f270fec2567
Tomek28611 <tomek286@yahoo.com>
2022-12-23 09:22:49 +0100

    offers, plan, rooms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
+import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
 
 function App() {
   return (
     <div className="">
         <Navbar />
         <Hero />
+        <Offers />
+        <Plan />
+        <Rooms />
     </div>
   );
 }
